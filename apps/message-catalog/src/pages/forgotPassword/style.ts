4ef04ab37e0591483dfe641b91dc2da15b034423
Kameshwaran Murugan <kameshwaran@crayond.co>
2023-03-23 11:01:41 +0530

    message catelog app added

---
+++ apps/message-catalog/src/pages/forgotPassword/style.ts
@@ -0,0 +1,13 @@
+import type { SxProps } from '@mui/material';
+
+interface ForgotPasswordStyleProps {
+  [key: string]: SxProps;
+}
+
+export const forgotPasswordStyle: ForgotPasswordStyleProps = {
+  rootSx: {
+    //  minHeight: '100vh',
+    // display: 'grid',
+    // placeItems: 'center',
+  },
+};
---
+++ apps/message-catalog/src/pages/login/index.tsx
@@ -0,0 +1,15 @@
+import { Login } from '@core/ui/components';
+import { Box } from '@mui/material';
+
+import { loginStyle } from './style';
+export default function LoginPage() {
+  return (
+    <Box
+      sx={{
+        ...loginStyle.rootSx,
+      }}
+    >
+      <Login />
+    </Box>
+  );
+}

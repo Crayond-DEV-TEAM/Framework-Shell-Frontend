947a65b1b51e50bd7735cec11645fbbb50492d40
harikrishnan <harikrishnan@crayond.co>
2023-10-18 16:19:34 +0530

    vite config updated for @crayond_dev/user-management-test

--- packages/user-management/vite.config.js
+++ packages/user-management/vite.config.js
@@ -49,6 +49,7 @@ export default defineConfig({
         '@mui/icons-material',
         '@mui/styled-engine',
         '@emotion/react',
+        '@mui/utils',
         '@emotion/styled',
         'notistack',
         'crayond-components-library-1',

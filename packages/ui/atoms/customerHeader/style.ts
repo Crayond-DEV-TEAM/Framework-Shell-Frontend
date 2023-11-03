e91f63c9a71e47b4e3715be02fdec488cd50f51b
Raman <raman@crayond.co>
2023-11-03 11:59:33 +0530

    changes:position style

--- packages/ui/atoms/customerHeader/style.ts
+++ packages/ui/atoms/customerHeader/style.ts
@@ -7,14 +7,14 @@ interface CustomerHeaderStyleProps {
 export const customerHeaderStyle: CustomerHeaderStyleProps = {
   rootSx: {
     padding: '4px 14px',
-    width: 'calc( 100% - 327px)',
-    position: 'fixed',
     top: 0,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
     backgroundColor: '#FFFFFF',
     zIndex: 999,
+    marginInline: '24px',
+    borderRadius: '8px',
   },
   imageBorder: {
     border: '1px solid #3B3B3B',

a39443ca7e04f7e640ecbe3c9ccfc3086c47e55d
narayanamoorthi <narayana@crayond.co>
2023-11-16 10:13:10 +0530

    reports delivery and list api integration

--- packages/store/alert-hub/reports.ts
+++ packages/store/alert-hub/reports.ts
@@ -7,6 +7,8 @@ import { useSlug } from '../common';
 export const useAlertReports = create<ReportInterface>((set, get) => ({
   reportCard: [],
   getTotalReports: [],
+  reportDelivery: [],
+  reportList: [],
   fetching: false,
   errorOnFetching: false,
 
@@ -76,6 +78,84 @@ export const useAlertReports = create<ReportInterface>((set, get) => ({
             // dataTable.push(obj)
           );
         }
+      })
+      .catch((err) => {
+        set({ errorOnFetching: true });
+        enqueueSnackbar(`Oops! Something went wrong, Try Again Later`, { variant: 'error' });
+      })
+      .finally(() => {
+        set({ fetching: false });
+      });
+    return false;
+  },
+  getReportDelivery: (index: any) => {
+    const slugId = '4a1035df-8acd-42a9-9f28-78a971e3deba';
+
+    set({ fetching: true, errorOnFetching: false });
+    const payload = {
+      reportFrom: index === 0 ? 'TODAY' : index === 1 ? 'THIS_WEEK' : index === 2 ? 'THIS_MONTH' : 'TODAY',
+      // dateRange: {
+      //   startAt: '2023-11-15T05:26:45.151Z',
+      //   endAt: '2023-11-15T05:26:45.151Z',
+      // },
+      additionalProp1: {},
+    };
+    httpRequest(
+      'post',
+      `https://dev-framework-api.crayond.com/api/v1/alertshub/reports/delivery-status`,
+      payload,
+      true,
+      undefined,
+      {
+        headers: {
+          slug: slugId,
+        },
+      },
+    )
+      .then((response) => {
+        let res = response?.data?.data;
+
+        console.log(res, 'res');
+
+        set({
+          reportDelivery: res,
+        });
+      })
+      .catch((err) => {
+        set({ errorOnFetching: true });
+        enqueueSnackbar(`Oops! Something went wrong, Try Again Later`, { variant: 'error' });
+      })
+      .finally(() => {
+        set({ fetching: false });
+      });
+    return false;
+  },
+
+  getReportList: (index: any) => {
+    const slugId = '4a1035df-8acd-42a9-9f28-78a971e3deba';
+
+    set({ fetching: true, errorOnFetching: false });
+    const payload = {
+      reportFrom: index === 0 ? 'TODAY' : index === 1 ? 'THIS_WEEK' : index === 2 ? 'THIS_MONTH' : 'TODAY',
+
+    };
+    httpRequest(
+      'post',
+      `https://dev-framework-api.crayond.com/api/v1/alertshub/reports/list`,
+      payload,
+      true,
+      undefined,
+      {
+        headers: {
+          slug: slugId,
+        },
+      },
+    )
+      .then((response) => {
+        let responseData = response?.data?.data;
+        set({
+          reportList: responseData,
+        });
       })
       .catch((err) => {
         set({ errorOnFetching: true });

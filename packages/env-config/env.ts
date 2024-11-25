67606d0d7afb167978fe781509fe9be2ca78d8bb
vengadesh_pandiyan <vengadesh@crayond.co>
2024-11-25 17:43:04 +0530

    Dev .enChanges

--- packages/env-config/env.ts
+++ packages/env-config/env.ts
@@ -3,7 +3,7 @@ export const envConfig = {
   api_url: import.meta.env.VITE_API_BASE_URL,
 
 
-  
+  // DEV-ONLY
   frame_work_shell_ui: import.meta.env.VITE_FRAMEWORK_SHELL_URL,
   message_catalog_root: import.meta.env.VITE_MESSAGE_CATALOGUE_URL,
   alert_hub: import.meta.env.VITE_ALERTHUB_URL,
